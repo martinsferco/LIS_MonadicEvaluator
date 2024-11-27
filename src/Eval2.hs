b53087cd058522d0f2ea02606829d43c79df7e0c
Martin Sferco <martinsferco.ms@gmail.com>
2024-11-27 18:42:23 -0300

    eval2 ready

--- src/Eval2.hs
+++ src/Eval2.hs
@@ -22,7 +22,7 @@ initEnv = M.empty
 
 -- Mónada estado, con manejo de errores
 newtype StateError a =
-  StateError { runStateError :: Env -> Either Error ( Pair a Env) }
+  StateError { runStateError :: Env -> Either Error (Pair a Env) }
 
 
 -- Para calmar al GHC
@@ -35,19 +35,27 @@ instance Applicative StateError where
 
 -- Ejercicio 2.a: Dar una instancia de Monad para StateError:
 instance Monad StateError where
-  return x = undefined
-  m >>= f = undefined
+  return x = StateError (\s -> Right (x :!: s))
+  m >>= f  = StateError (\s -> runStateError m s >>= \(x :!: s') -> 
+                               runStateError (f x) s')
 
 -- Ejercicio 2.b: Dar una instancia de MonadError para StateError:
--- COMPLETAR
+instance MonadState StateError where
+  lookfor v = StateError (\s -> case M.lookup v s of
+                                 Nothing -> Left UndefVar
+                                 Just x  -> return (x :!: s))
+
+  update v i = StateError (\s -> return (() :!: M.insert v i s))
 
 -- Ejercicio 2.c: Dar una instancia de MonadState para StateError:
--- COMPLETAR
+instance MonadError StateError where
+  throw e = StateError (\_ -> Left e)
 
 -- Ejercicio 2.d: Implementar el evaluador utilizando la monada StateError.
 -- Evalua un programa en el estado nulo
 eval :: Comm -> Either Error Env
-eval = undefined
+eval p = runStateError (stepCommStar p) initEnv >>= \p ->
+         return (snd p)
 
 -- Evalua multiples pasos de un comando, hasta alcanzar un Skip
 stepCommStar :: (MonadState m, MonadError m) => Comm -> m ()
@@ -56,9 +64,50 @@ stepCommStar c    = stepComm c >>= \c' -> stepCommStar c'
 
 -- Evalua un paso de un comando
 stepComm :: (MonadState m, MonadError m) => Comm -> m Comm
-stepComm = undefined
+stepComm (Let v e)            = do n <- evalExp e
+                                   update v n
+                                   return Skip
+
+stepComm (Seq Skip c1)        = return c1
+stepComm (Seq c0 c1)          = do c0' <- stepComm c0
+                                   return (Seq c0' c1)
+
+stepComm (IfThenElse b c0 c1) = do vb <- evalExp b
+                                   if vb then return c0 else return c1
+
+stepComm (Repeat b c)         = return (Seq c c')
+                                where c' = (IfThenElse b (Repeat b c) Skip)
 
 -- Evalua una expresion
 evalExp :: (MonadState m, MonadError m) => Exp a -> m a
-evalExp = undefined
+evalExp (Const n)        = return n
+evalExp (Var x)          = lookfor x
+
+evalExp (UMinus e)       = evalUnary (negate)  e 
+evalExp (Plus e0 e1)     = evalBinary (+)   e0 e1
+evalExp (Minus e0 e1)    = evalBinary (-)   e0 e1
+evalExp (Times e0 e1)    = evalBinary (*)   e0 e1
+evalExp (Div e0 e1)      = evalBinary (div) e0 e1
+
+
+evalExp (VarInc x)       = do n <- lookfor x
+                              update x (suc n)
+                              return (suc n)
+
+evalExp (VarDec x)       = do n <- lookfor x
+                              update x (pred n)
+                              return (pred n)
+
+evalExp BTrue            = return True
+evalExp BFalse           = return False
+
+evalExp (Lt e0 e1)       = evalBinary (<) e0 e1
+evalExp (Gt e0 e1)       = evalBinary (>) e0 e1
+
+evalExp (And e0 e1)      = evalBinary (&&) e0 e1
+evalExp (Or e0 e1)       = evalBinary (||) e0 e1
+evalExp (Not e)          = evalUnary (not) e
+evalExp (Eq e0 e1)       = evalBinary (==) e0 e1 
+evalExp (NEq e0 e1)      = evalBinary (/=) e0 e1
+
 
