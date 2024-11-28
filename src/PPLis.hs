991403e408b6f059bfd127630d5ed0f02c2af753
Martin Sferco <martinsferco.ms@gmail.com>
2024-11-28 10:28:08 -0300

    pp ready

--- src/PPLis.hs
+++ src/PPLis.hs
@@ -27,9 +27,8 @@ pExp (Gt  a b     ) = pExp a <+> text ">" <+> pExp b
 pExp (And a b     ) = pExp a <+> text "&&" <+> pExp b
 pExp (Or  a b     ) = pExp a <+> text "||" <+> pExp b
 pExp (Not b       ) = text "!" <+> pExp b
--- Ejercicio 2
-pExp (EAssgn x  e ) = parens $ pVar x <+> text "=" <+> pExp e
-pExp (ESeq   e1 e2) = pExp e1 <> comma <+> pExp e2
+pExp (VarDec x    ) = pVar x <+> text "--"
+pExp (VarInc x    ) = pVar x <+> text "++"
 
 pComm :: Comm -> Doc
 pComm Skip        = text "skip"
